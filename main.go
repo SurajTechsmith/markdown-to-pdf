d786c30eda9f6c214635a4944acda51cb11d201e
SurajTechsmith <surajyadav27092001@gmail.com>
2026-02-22 20:46:35 +0530

    Added fyne UI

--- main.go
+++ main.go
@@ -1,30 +1,94 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"md_to_pdf/converter"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/widget"
+
 	"os"
+	"path/filepath"
 	"time"
 )
 
 func main() {
-	start := time.Now()
-	input := flag.String("in", "", "Path to input file")
-	output := flag.String("out", "", "Path to output pdf file (optional)")
+	a := app.New()
+	w := a.NewWindow("Markdown to PDF")
 
-	flag.Parse()
+	var inputPath string
+	var outputPath string
 
-	if *input == "" {
-		fmt.Println("Please provide input file using -in flag")
-		os.Exit(1)
-	}
+	inputLabel := widget.NewLabel("No file selected")
+	outputLabel := widget.NewLabel("No output selected")
+	exePath, _ := os.Executable()
+	exeDir := filepath.Dir(exePath)
+	selectInput := widget.NewButton("Select Markdown", func() {
+		d := dialog.NewFileOpen(func(file fyne.URIReadCloser, err error) {
+			if err != nil || file == nil {
+				return
+			}
+			inputPath = file.URI().Path()
+			inputLabel.SetText(inputPath)
+		}, w)
+		uri, err := storage.ListerForURI(storage.NewFileURI(exeDir))
+		if err == nil {
+			d.SetLocation(uri)
+		}
 
-	err := converter.Convert(*input, *output)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
-	}
-	end := time.Since(start)
-	fmt.Println("PDF created successfully hurray : ", end)
+		d.Show()
+	})
+
+	selectOutput := widget.NewButton("Select Output Location", func() {
+		dialog.NewFileSave(func(file fyne.URIWriteCloser, err error) {
+			if err != nil || file == nil {
+				return
+			}
+			outputPath = file.URI().Path()
+			outputLabel.SetText(outputPath)
+		}, w).Show()
+	})
+
+	convertBtn := widget.NewButton("Convert", func() {
+		if inputPath == "" || outputPath == "" {
+			dialog.ShowInformation("Error", "Please select input and output", w)
+			return
+		}
+
+		if filepath.Ext(outputPath) != ".pdf" {
+			outputPath += ".pdf"
+		}
+
+		start := time.Now()
+
+		err := converter.Convert(inputPath, outputPath)
+		if err != nil {
+			dialog.ShowError(err, w)
+			return
+		}
+
+		elapsed := time.Since(start)
+
+		dialog.ShowInformation(
+			"Success",
+			fmt.Sprintf("PDF created successfully!\nTime taken: %v", elapsed),
+			w,
+		)
+	})
+
+	content := container.NewVBox(
+		selectInput,
+		inputLabel,
+		selectOutput,
+		outputLabel,
+		convertBtn,
+	)
+
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(600, 500))
+	w.ShowAndRun()
 }
